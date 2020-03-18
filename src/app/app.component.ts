fd3ae7c8a3d8ecf5b44e4c6d325f7ab361af94a5
David Mayes <davidrmayes@gmail.com>
2020-03-17 22:14:55 -0600

    Added http problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
 @Component({
     selector: 'app-root',
@@ -12,26 +11,77 @@ export class AppComponent implements OnInit {
     // ** To start application, run 'npm start' **
 
     title = 'SandBox';
+    imgUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTSzrAXk1k39T9k5i8Z81CYkBAMtZDIHMXptgXlLkY6a0L08Ixl';
 
 
     ngOnInit(): void {
-        // NESTED COMPONENTS DAY
 
         // TOY PROBLEMS - Solve the problems below as effeciently as possible.
         // Not all problems can be solved with one line of code.
 
+
+        // HTTP DAY
+        const alphabet = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+        // 1. Given the array of numbers below, console log the associated letters from above to make a sentense.
+        const numbers = [23, 5, 12, 3, 15, 13, 5, 0, 20, 15, 0, 3, 12, 1, 19, 19];
+
+        // 2. Create a div in the app.component.html file and use interpolation to display the title variable from above.
+
+        // 3. Create a variable within AppComponent called todaysDate and set it equal to today's date (hint use 'new Date()').
+        //    Create a div in the html to display the date and use a pipe(s) to make it look like "WEDNESDAY, MARCH 18, 2020"
+
+        // 4. Using the 'imgUrl' variable from above display that image in the browser.
+
+        // 5. Use Angular's `ngClass` property to style the background of your div from problem #2 to be blue if there are more
+        //    than four characters in the title variable
+
+
+        // ANGULAR PRACTICE
+
+        // 1. Import HttpClientModule into the app.module.ts file
+        // 2. Create a 'MealService'
+        // 3. Inject the HttpClient into 'MealService'
+        // 4. Create a method that makes an api call to 'https://www.themealdb.com/api/json/v1/1/search.php?f=a' and
+        //    returns the value as an observable. The method should accept a parameter that can be used to make
+        //    'a' at the end of the api url dynamic.
+        // 5. In the app.component.ts file within the 'ngOnInit' use a 'combineLatest' to make two separate calls
+        //    to your 'MealService' to call your api method twice using two different search letters.
+        // 6. Filter the results down to meals that are either Desserts or are American
+        // 7. Set variables in the app.component.ts file equal to the desserts and the meals that are American.
+        // 7. In the app.component.html file, display two lists: one for desserts and one for meals that are American.
+        // 8. In each list item display the meal id, the meal name, the meal area, and the meal thumbnail.
+
+
+
+
+
+
+        // NESTED COMPONENTS DAY
+
         const stringCount = 'Can you count the amount of characters in this string?';
         // 1. Console log the amount of characters in the stringCount variable above.
+        // console.log(stringCount.length);
 
         // 2. Console log the amount of words that exist in stringCount
+        // console.log(stringCount.split(' ').length);
 
         // 3. Console log the amount of 'C/c' characters in the stringCount variable above.
+        const newArr = [];
+        const secondArr = stringCount.split('');
+        secondArr.forEach(c => {
+            if (c.toLowerCase() === 'c') {
+                newArr.push(c);
+            }
+        });
+        // console.log(newArr.length);
 
         const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 37, 36, 45, 48, 55];
         // 4. Console log all values from numArray that are evenly divided by 3.
+        // console.log(numArray.filter(x => x % 3 === 0));
 
         // 5. Console log the result array after removing every value that is evenly divided by 5.
-
+        // console.log(numArray.filter(num => num % 5 !== 0));
 
 
         // ANGULAR PRACTICE
